bdec9c31f00c322cc87c27145a507e1e6aa3c0b9
Ildar Mindiyarov <77970650+AnMSLbR@users.noreply.github.com>
2021-02-18 15:23:30 +0500

    Feature-21. Добавление тестового метода CheckPluginAccessFake() и методов CheckResultWriteBooksFake(), CheckResultReadBooksFake(), ReturnListOfStrings() в класс DataSourceAccessTests.

--- Library.Tests/DataSourceAccessTests.cs
+++ Library.Tests/DataSourceAccessTests.cs
@@ -105,5 +105,65 @@ namespace Library.Tests
 
             }
         }
+
+        [TestMethod]
+        public void CheckPluginAccessFake()
+        {
+            CheckResultWriteBooksFake();
+            CheckResultReadBooksFake();
+        }
+
+        private void CheckResultWriteBooksFake()
+        {
+            var mock = new Mock<IDataBase>();
+            List<string> listOfInserts = new List<string>();
+            for (int i = 0; i < _listOfBooks.Count; i++)
+                listOfInserts.Add($"INSERT INTO Books (Id, Author, Title, ISDN, Price) VALUES ({i + 1},'{_listOfBooks[i][0]}','{_listOfBooks[i][1]}'," +
+                    $"'{_listOfBooks[i][2]}',{Convert.ToDecimal(_listOfBooks[i][3])})");
+            foreach (string insertQuery in listOfInserts)
+                mock.Setup(x => x.Modify(insertQuery));
+
+            _plugin = new DataSourceAccess(mock.Object);
+
+            _plugin.WriteBooks(_listOfBooks);
+
+            mock.VerifyAll();
+        }
+
+        private void CheckResultReadBooksFake()
+        {
+            var mock = new Mock<IDataBase>();
+            mock.Setup(x => x.Retrieve("SELECT * FROM Books"))
+                .Returns(ReturnListOfStrings());
+
+            _plugin = new DataSourceAccess(mock.Object);
+            var expected = _listOfBooks;
+
+            var actual = _plugin.ReadBooks();
+
+            Assert.IsTrue(actual != null, "Ошибка чтения: Метод возвращает 'null'");
+            Assert.AreEqual(expected.Count, actual.Count, "Ошибка чтения: Возвращено неправильное число книг");
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.IsTrue(expected[i][0] == actual[i][0], ($"Ошибка чтения: Поле 'Автор' {i + 1}-й книги прочитано некорректно"));
+                Assert.IsTrue(expected[i][1] == actual[i][1], ($"Ошибка чтения: Поле 'Название' {i + 1}-й книги прочитано некорректно"));
+                Assert.IsTrue(expected[i][2] == actual[i][2], ($"Ошибка чтения: Поле 'ISDN' {i + 1}-й книги прочитано некорректно"));
+                Assert.IsTrue(expected[i][3] == actual[i][3], ($"Ошибка чтения: Поле 'Цена' {i + 1}-й книги прочитано некорректно"));
+            }
+        }
+
+        private List<string> ReturnListOfStrings()
+        {
+            List<string> listOfStrings = new List<string>();
+            int id = 1;
+            foreach (List<string> Book in _listOfBooks)
+            {
+                listOfStrings.Add($"{id}");
+                listOfStrings.AddRange(Book);
+                id++;
+            }
+            return listOfStrings;
+        }
+
     }
 }
